94803d78cf04b9de8bd874c9b8095d9931546d0c
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-05-07 15:14:44 +0530

    changesin navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,9 +35,10 @@ if(searchQuery.length>0){
 
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
+                <NavLink className='border border-gray-300 px-3 py-1 rounded-full text-xs cursor-pointer opacity-80' to='/seller'>Seller Dashboard</NavLink>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/products'>All Product</NavLink>
-                <NavLink to='/'>Contact</NavLink>
+                
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input onChange={(e)=>setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />

d13c9e408841dec8d2f8c0569e3370568e6287fb
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-05-07 15:18:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const port=process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins=['http://localhost:5173'];
+const allowedOrigins=['https://greencart-frontend-sandy.vercel.app'];
 
 app.post('/stripe', express.raw({type:'application/json'}),stripeWebhooks)
 

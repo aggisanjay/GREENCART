3e78f7c9cc465802cbc8db701e43f13e0830acd0
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-05-07 12:38:43 +0530

    changes in productCard

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -39,7 +39,7 @@ const Orders = () => {
                             {order.items.map((item, index) => (
                                 <div key={index} className="flex flex-col ">
                                     <p className="font-medium">
-                                        {item.product.name}{" "} <span className="text-primary">x {item.quantity}</span>
+                                        {item.product?.name}{" "} <span className="text-primary">x {item.quantity}</span>
                                     </p>
                                 </div>
                             ))}
